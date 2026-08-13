f48dfd32bd118e999a6c817e0f12999852b77bb5
Pavel Soloviev <p.soloviev@mixvel.com>
2026-08-13 12:37:38 +0300

    fix configs

--- Options/EmailOptions.cs
+++ Options/EmailOptions.cs
@@ -3,7 +3,7 @@
 public class EmailOptions
 {
     public string SmtpServer { get; set; } = "smtp.gmail.com";
-    public int SmtpPort { get; set; } = 587;
+    public int SmtpPort { get; set; } = 465;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public bool EnableSsl { get; set; } = true;
